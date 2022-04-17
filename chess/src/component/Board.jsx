8f778ecce0e2093873d177e704d2059d3ce09611
Avinash <avinash1234sharma1@gmail.com>
2022-04-17 23:04:27 +0530

    logic of Pawn

--- chess/src/component/Board.jsx
+++ chess/src/component/Board.jsx
@@ -8,8 +8,9 @@ import Cell from './Cell';
 function Board() {
 
     const [allWhitefillPosition, setWhiteAllFillPosition] = useState([]);
+    const [allBlackfillPosition, setBlackAllFillPosition] = useState([]);
     const [possiblePosition, setpossiblePosition] = useState([]);
-    const [isCurrentChance, setCurrentChance] = useState('White');
+    const [isCurrentChance, setCurrentChance] = useState('Black');
     const [focusIdentity, setFocusIndentity] = useState('');
     const [whiteBoard, setWhiteBoard] = useState({
         'PawnWhite': [[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]],
@@ -30,23 +31,120 @@ function Board() {
     })
 
 
-    function logicalChess(identity, index) {
-        const newWhiteBoard = whiteBoard;
-        const newBlackBoard = blackBoard;
-        const [newRow, newCol] = index;
-        const possbileChanges = [];
+    const chessCoreLogic = (index, identity) => {
+
+        if (isCurrentChance === 'White') {
+
+
+            if (identity === 'PawnWhite') {
+                const pawnLogic = () => {
+                    let [row, col] = index;
+                    const whitePossibleAheadChances = allWhitefillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row + 1 && newCol === col);
+                    });
+
+                    const blackPossibleAheadChances = allBlackfillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row + 1 && newCol === col);
+                    });
+
+                    const whitePossibleNewAheadChances = [];
+                    if (whitePossibleAheadChances.length === 0 && blackPossibleAheadChances.length === 0) whitePossibleNewAheadChances.push([row + 1, col]);
+
+                    const whitePossbileCrossChances = allWhitefillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row + 2 && newCol === col)
+                    });
+
+                    const blackPossibleCrossChances = allBlackfillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row + 2 && newCol === col);
+                    });
+                    const whitePossibleNewCrossChances = [];
+
+                    if ((whitePossbileCrossChances.length === 0 && row === 1 && blackPossibleCrossChances.length === 0)) whitePossibleNewCrossChances.push([row + 2, col]);
+                    console.log(whitePossbileCrossChances.length);
+
+
+                    const blackPossibleChances = allBlackfillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row + 1 && newCol === col + 1) || (newRow === row + 1 && newCol === col - 1);
+                    })
+                    // console.log(whitePossibleAheadChances, whitePossibleAheadChances, blackPossibleChances)
+
+                    return [...whitePossibleNewAheadChances, ...whitePossibleNewCrossChances, ...blackPossibleChances]
 
 
 
-        if (identity === 'PawnBlack') {
-            const nextMovePawn = newRow + 1;
-
-            // possbileChanges.push([newMovePawn, newCol]);
 
 
+                }
+                return pawnLogic();
+            }
 
+            if(identity==="KnightWhite") {
+                 
+            }
         }
+
+        console.log(isCurrentChance, identity);
+        if (isCurrentChance === 'Black') {
+            if (identity === 'PawnBlack') {
+                console.log(isCurrentChance, identity);
+                const pawnLogic = () => {
+                    let [row, col] = index;
+                    const blackPossibleAheadChances = allBlackfillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row - 1 && newCol === col);
+                    });
+
+                    const whitePossibleAheadChances = allWhitefillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row - 1 && newCol === col);
+                    });
+
+                    console.log(whitePossibleAheadChances.length);
+
+                    const blackPossibleNewAheadChances = [];
+                    if (blackPossibleAheadChances.length === 0 && whitePossibleAheadChances.length === 0) blackPossibleNewAheadChances.push([row - 1, col]);
+
+                    const blackPossbileCrossChances = allBlackfillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row - 2 && newCol === col)
+                    });
+
+                    const whitePossbileCrossChances = allWhitefillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row - 2 && newCol === col)
+                    });
+
+
+                    const blackPossibleNewCrossChances = [];
+
+
+                    if (blackPossbileCrossChances.length === 0 && row === 6 && whitePossbileCrossChances.length === 0) blackPossibleNewCrossChances.push([row - 2, col]);
+
+                    const whitePossibleChances = allWhitefillPosition.filter((item) => {
+                        const [newRow, newCol] = item;
+                        return (newRow === row - 1 && newCol === col + 1) || (newRow === row - 1 && newCol === col - 1);
+                    })
+                    // console.log(whitePossibleAheadChances, whitePossibleAheadChances, blackPossibleChances)
+
+                    return [...blackPossibleNewAheadChances, ...blackPossibleNewCrossChances, ...whitePossibleChances]
+                }
+                return pawnLogic();
+            }
+        }
+
+
+        console.log(allWhitefillPosition);
+        console.log(allBlackfillPosition);
+
+
+
     }
+
 
 
 
@@ -57,13 +155,16 @@ function Board() {
 
     let [board, setBoard] = useState(Array(8).fill(Array(8).map((item) => item.fill(null))));
     let [focusIndex, setFocusIndex] = useState([]);
-    console.log(board)
-    console.log(focusIndex);
+    // console.log(board)
+    // console.log(focusIndex);
 
     const changeFocus = (index, identity) => {
-        console.log(index, identity);
+        // console.log(index, identity);
         setFocusIndex([...index]);
-        setpossiblePosition([[2, 4], [3, 4], [5, 3]]);
+        let possibleChances = chessCoreLogic(index, identity);
+        console.log(possibleChances);
+
+        setpossiblePosition([...possibleChances]);
         setFocusIndentity(identity);
 
     }
@@ -76,17 +177,17 @@ function Board() {
         if (isCurrentChance === 'White') {
 
             if (current === "Black") {
-                console.log(current);
+                // console.log(current);
                 const duplicateBlackBoard = [...blackBoard[identity]];
                 // console.log(row, col);
                 // console.log(duplicateWhiteBoard);
 
                 const duplicateNewBlackBoard = duplicateBlackBoard.filter((item) => {
                     let [newRow, newCol] = item
-                    console.log(newRow == row && newCol == col);
+                    // console.log(newRow == row && newCol == col);
                     return !(newRow === row && newCol === col);
                 })
-                console.log(duplicateNewBlackBoard)
+                // console.log(duplicateNewBlackBoard)
                 setBlackBoard((perv) => ({
                     ...perv,
                     [identity]: duplicateNewBlackBoard
@@ -123,7 +224,7 @@ function Board() {
             console.log("Black ")
 
             if (current === "White") {
-                console.log(current);
+                // console.log(current);
                 const duplicateWhiteBoard = [...whiteBoard[identity]];
                 // console.log(row, col);
                 // console.log(duplicateWhiteBoard);
@@ -132,7 +233,7 @@ function Board() {
                     let [newRow, newCol] = item
                     return !(newRow === row && newCol === col);
                 })
-                console.log(duplicateNewWhiteBoard)
+                // console.log(duplicateNewWhiteBoard)
                 setWhiteBoard((perv) => ({
                     ...perv,
                     [identity]: duplicateNewWhiteBoard
@@ -191,11 +292,13 @@ function Board() {
     // providing position of white pieces of chess into board
     useEffect(() => {
         let duplicateBoard = Array(8).fill().map(() => Array(8).fill(null));
-
+        const duplicateWhiteBoard = [];
+        const duplicateBlackBoard = [];
         for (let key in whiteBoard) {
 
             whiteBoard[key].forEach((item) => {
                 // console.log(item)
+                duplicateWhiteBoard.push(item);
                 let [row, col] = item;
 
 
@@ -214,6 +317,7 @@ function Board() {
 
             blackBoard[key].forEach((item) => {
                 // console.log(item)
+                duplicateBlackBoard.push(item);
                 let [row, col] = item;
                 // console.log(row, col);
                 // console.log(key);
@@ -226,8 +330,10 @@ function Board() {
             })
 
         }
-
         setBoard(duplicateBoard);
+        setBlackAllFillPosition(duplicateBlackBoard);
+        setWhiteAllFillPosition(duplicateWhiteBoard);
+
 
     }
 
